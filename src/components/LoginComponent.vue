1e03157c16343f1f12cf09a3ca45d1fcc908bb36
mahfuz1418 <mahfujul.dev@gmail.com>
2025-08-20 23:53:47 +0600

    Vue animation

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="d-flex justify-content-center align-items-center vh-100 bg-light">
     <div class="card shadow p-4" style="width: 400px;">
+
+      <div class="box" :class="{'box-right' : boxRight}"></div>
+      <div class="d-flex mt-4">
+        <button class="btn btn-sm btn-danger me-2" @click="boxRight = true">Move Right</button>
+        <button class="btn btn-sm btn-danger"  @click="boxRight = false">Move Left</button>
+      </div>
+      <br>
+      <hr>
+      <Transition name="showhide">
+        <div class="box1"  v-if="showBox"></div>
+      </Transition>
+      <button class="btn btn-sm btn-success mt-3" @click="showBox = !showBox">Show / Hide</button>
+
       <h2 class="mb-4 text-center">Login</h2>
       <form @submit.prevent="handleForm">
         <!-- Email Field -->
@@ -17,7 +30,7 @@
 
 
         <!-- Submit Button -->
-        <button type="submit" class="btn btn-primary w-100">Submit</button>
+        <button type="submit" class="btn btn-sm btn-danger w-100">Submit</button>
       </form>
     </div>
   </div>
@@ -29,7 +42,9 @@ export default {
             formData: {
                 email: "",
                 password: "",
-            }
+              },
+            boxRight: false,
+            showBox: false
         }
     },
     methods: {
@@ -57,6 +72,72 @@ export default {
     },
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    .box{
+      width: 60px;
+      height: 60px;
+      background-color: greenyellow;
+      transition: all 1s;
+    }
+
+    .box-right{
+      margin-left: 222px;
+    }
+
+    .box1{
+      width: 60px;
+      height: 60px;
+      background-color: greenyellow;
+      transition: all 1s;
+      /* animation: showhide 1s ease-in; */
+    }
+
+    @keyframes showhide {
+      0%{
+        opacity: 0;
+        transform: scale(0.5);
+      }
+
+      50%{
+        opacity: 0.5;
+        transform: scale(1.2);
+      }
+
+      100%{
+        opacity: 1;
+        transform: scale(1);
+      }
+    }
+
+    .showhide-enter-active{
+      /* transition: all .5; */
+      animation: showhide 1s ease-in;
+    }
+
+    .showhide-enter-from{
+      /* opacity: 0;
+      transform: scale(0.5); */
+    }
+
+    .showhide-enter-to{
+      /* opacity: 1;
+      transform: scale(1); */
+
+    }
+
+    .showhide-leave-active{
+      animation: showhide 1s ease-in reverse;
+      /* transition: all 0.5; */
+    }
+
+    .showhide-leave-from{
+      /* opacity: 1;
+      transform: scale(1); */
+    }
+
+    .showhide-leave-to{
+      /* opacity: 0;
+      transform: scale(0.5); */
+
+    }
 </style>
